fe01047461e5c783ad112d4845cd3e4c8476200c
surpreet.mundra@gmail.com <surpreet.mundra@gmail.com>
2018-08-29 16:15:04 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -22,9 +22,9 @@ class Triangle
   def valid_triangle
     all_sides_greater_than_zero = side1<=0 && side2<=0 && side3<=0
     all_sides_join = side1+side2>side3 || side1+side3>side2 || side2+side3>side1
-    if all_sides_join && all_sides_greater_than_zero != true
+    if all_sides_join && all_sides_greater_than_zero =! true
      raise TriangleError
-   end 
+   end
   end
 
 
