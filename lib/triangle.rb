9209daf9e767824be7823105c2a3ac5e06a68ee3
surpreet.mundra@gmail.com <surpreet.mundra@gmail.com>
2018-08-29 16:07:02 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,7 +11,7 @@ class Triangle
 
   def kind
     valid_triangle
-    if side1 == side2 == side3
+    if side1 == side2 && side2 == side3
       :equilateral
     elsif side1 == side2 || side2 == side3 || side1 == side3
       :isosceles
