a9a88b08239a492acd206bebdb94dfcae60fa59a
surpreet.mundra@gmail.com <surpreet.mundra@gmail.com>
2018-08-29 17:04:30 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -23,7 +23,6 @@ class Triangle
 
     check1 = side1>0 && side2>0 && side3>0
     check2 = side1+side2>side3 && side1+side3>side2 && side2+side3>side1
-    binding.pry
 
     if check1 == false || check2 == false
      raise TriangleError
