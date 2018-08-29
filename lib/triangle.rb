b650c9b4ea5b1b99fcd3d1e46b98ded7c4b41708
surpreet.mundra@gmail.com <surpreet.mundra@gmail.com>
2018-08-29 16:32:56 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -22,7 +22,7 @@ class Triangle
   def kind
     check1 = if side1<=0 && side2<=0 && side3<=0
       false
-    end 
+    end
     check2 = if side1+side2>side3 || side1+side3>side2 || side2+side3>side1
       true
     end 
