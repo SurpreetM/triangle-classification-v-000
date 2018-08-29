cecad88d1a191250bf563ecbaeb2f898527dd634
surpreet.mundra@gmail.com <surpreet.mundra@gmail.com>
2018-08-29 16:06:24 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,5 +1,5 @@
 class Triangle
-  
+
   attr_accessor :side1, :side2, :side3
 
   def initialize (side1, side2, side3)
@@ -7,7 +7,7 @@ class Triangle
     @side2 = side2
     @side3 = side3
     sides_array = [side1, side2, side3]
-  end 
+  end
 
   def kind
     valid_triangle
@@ -15,23 +15,23 @@ class Triangle
       :equilateral
     elsif side1 == side2 || side2 == side3 || side1 == side3
       :isosceles
-    else 
+    else
       :scalene
-    end 
-  end 
+    end
+  end
 
   def valid_triangle
-    if side1>0 && side2>0 && side3>0 
-      if side1+side2>side3 || side1+side3>side2 || side2+side3>side1 
-        true 
+    if side1>0 && side2>0 && side3>0
+      if side1+side2>side3 || side1+side3>side2 || side2+side3>side1
+        true
       else raise TriangleError
-      end 
-    end 
-  end 
-    
-      
+      end
+    end
+  end
+
+
 
   class TriangleError < StandardError
   end
-  
+
 end
